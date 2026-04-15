6e5c4431b1dc840633b288b9c226a88e5bee5187
duoladeAmeng <1107128204@qq.com>
2026-04-15 12:32:22 +0800

    修改SpringAI依赖版本

--- src/main/java/com/dyc/AiRobotApplication.java
+++ src/main/java/com/dyc/AiRobotApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AiRobotApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AiRobotApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AiRobotApplication.class, args);
+    }
 
 }
--- src/test/java/com/dyc/AiRobotApplicationTests.java
+++ src/test/java/com/dyc/AiRobotApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class AiRobotApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
